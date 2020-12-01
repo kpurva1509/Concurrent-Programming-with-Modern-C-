ee3bf1fcafdc3f6783af2737c1905b17327ce65d
Purva Kulkarni <37818444+kpurva1509@users.noreply.github.com>
2020-12-01 09:16:21 -0500

    Create headers.hpp

---
+++ Multithreaded Parallel Accumulate/headers.hpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <thread>
+#include <chrono>
+#include <cmath>
+#include <cstdlib>
+#include <cstdio>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <map>
+#include <functional>
+#include <algorithm>
+#include <numeric>
+#include <condition_variable>
+#include <future>
+
+using std::cout;
+using std::endl;
+using std::vector;
+using std::string;
+using std::function;
+using std::map;
+// using std::chrono;
+using std::thread;
+using std::ostream;
+// using std:ostringstream;
+
+#define MIN_BLOCK_SIZE 1000
